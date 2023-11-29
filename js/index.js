76c67cc0b2075bf33fd54b983e6083948f92a6a4
juan manuel blazquez <blazquezjuanmanuel@gmail.com>
2023-11-29 20:08:12 -0300

    Estructura básica html y array con productos JS

---
+++ js/index.js
@@ -0,0 +1,20 @@
+let productos = [
+    {
+    nombre: 'Cerveza',
+    precio: 1000},
+    {
+    nombre: 'Fernet',
+    precio: 2000},
+    {
+    nombre: 'Gin',
+    precio: 3000},
+    {
+    nombre: 'Ron',
+    precio: 2500},
+    {
+    nombre: 'Whisky',
+    precio: 5000}
+
+]
+
+console.log(productos)
