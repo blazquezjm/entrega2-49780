80759ed96fe5101831b8c8d99a85a1f771f25bd8
juan manuel blazquez <blazquezjuanmanuel@gmail.com>
2023-12-08 17:36:34 -0300

    agregado el comienzo de la función para que pida al usuario el producto y la cantidad

--- js/index.js
+++ js/index.js
@@ -1,20 +1,35 @@
 let productos = [
     {
-    nombre: 'Cerveza',
-    precio: 1000},
+        nombre: 'cerveza',
+        precio: 1000
+    },
     {
-    nombre: 'Fernet',
-    precio: 2000},
+        nombre: 'fernet',
+        precio: 2000
+    },
     {
-    nombre: 'Gin',
-    precio: 3000},
+        nombre: 'gin',
+        precio: 3000
+    },
     {
-    nombre: 'Ron',
-    precio: 2500},
+        nombre: 'ron',
+        precio: 2500
+    },
     {
-    nombre: 'Whisky',
-    precio: 5000}
+        nombre: 'whisky',
+        precio: 5000
+    }
+];
 
-]
+let totalPedido = 0;
 
-console.log(productos)
+do {
+
+    function calcularTotal() {
+        const nombreProducto = prompt('Los productos a seleccionar son: cerveza, whisky, fernet, gin, ron. \n Ingrese el nombre del producto que quiere adquirir:');
+        const cantidad = parseInt(prompt('Ingrese la cantidad que desea:'));
+
+        const productoEncontrado = productos.find(producto => producto.nombre.toLowerCase() === nombreProducto.toLowerCase());
+
+    }
+}while();
